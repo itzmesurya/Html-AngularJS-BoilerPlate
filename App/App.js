dd65e08750bed8bbdad5bdc466d395efb7419993
Surya <itzmesurya@gmail.com>
2017-11-30 16:26:49 +0530

    Added Angular Formly

--- App/App.js
+++ App/App.js
@@ -1,6 +1,16 @@
 (function ()
 {
     ; Global_NS = {
-        toastrApp: angular.module('toastrApp', [])
+        toastrApp: angular.module('toastrApp', ['formly', 'formlyBootstrap'])
     };
+
+    Global_NS.toastrApp.config(function(formlyConfigProvider) {
+        formlyConfigProvider.setType({
+            name:'inputCustom',
+            template:`<label ng-bind="to.label"></label><span ng-if="to.required">*</span>
+            <input class="form-control" type="{{to.type}}" ng-model="model[options.key]" ng-required="to.required" ng-disabled="to.disabled" />
+            `,
+            wrapper: [ 'bootstrapHasError']
+        })
+    });
 })();
--- App/MainPage/MainPageController.js
+++ App/MainPage/MainPageController.js
@@ -1,11 +1,59 @@
-(function ()
-{
-    debugger;
+(function () {
+    // debugger;
     Global_NS.toastrApp.controller('MainPage', ['$scope', 'MainService', MainPage]);
-    function MainPage($scope, MainService)
-    {
+
+    function MainPage($scope, MainService) {
         var vm = this;
-        vm.testVal = 'Hello World!';
+        vm.testVal = 'Hello World from Main Page!';
         vm.pageName = MainService.PageName;
+
+        vm.fields = [{
+            key: 'emailRequired',
+            type: 'checkbox',
+            templateOptions: {
+                label: 'emailRequired'
+            }
+
+        }
+        ,{
+            key: 'hideCustomInput',
+            type: 'checkbox',
+            templateOptions: {
+                label: 'hideCustomInput'
+            }
+
+        }, {
+            key: 'email',
+            type: 'input',
+            templateOptions: {
+                type: 'email',
+                label: 'Email address',
+                placeholder: 'Enter email'
+            },
+            expressionProperties:{
+                'templateOptions.required':'model.emailRequired'
+            },
+            validation:{
+                messages:{
+                    required:'/"Email is required/"'
+                }
+            }
+
+        },
+    {
+        type:'inputCustom',
+        key:'CustomName',
+        templateOptions:{
+            label:'Custom Name'           
+        },
+        expressionProperties:{
+            'templateOptions.required':'model.emailRequired',
+            'templateOptions.setDefaultValue':'model.CustomName = model.CustomName?model.CustomName:"Pradeep"'
+        },
+        hideExpression:'model.hideCustomInput'
+    }];
+    vm.actualFiels=angular.copy(vm.fields);
+        vm.options = {};
+        vm.model = {'email':'pradeep@gmail.com',CustomName:'Srinivas'};
     }
 })();
